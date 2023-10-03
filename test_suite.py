0d517380a31cf30a7db18af2cb862dd94d57f817
jasonhsu8 <jace.hsu@gmail.com>
2023-10-03 11:02:57 +0100

    test add_numbers

--- test_suite.py
+++ test_suite.py
@@ -1,1 +1,22 @@
-print("YOOOOO")
+import unittest
+
+def add_numbers(a, b):
+    return a + b
+
+class testFunction(unittest.TestCase):
+
+    def test_addition(self):
+        result = add_numbers(4, 5)
+        self.assertEqual(result, 9)
+    
+    def test_addition_negative_numbers(self):
+        result = add_numbers(6, -2)
+        self.assertEqual(result, 4)
+
+    def test_addition_float_numbers(self):
+        result = add_numbers(1.5, 2.5)
+        self.asserEqual(result, 4.0, places=2)
+
+if __name__ == '__main__':
+    unittest.main()
+
