8c31fe0eddbab9e528a31e883efe961001c535c9
jasonhsu8 <jace.hsu@gmail.com>
2023-10-05 11:05:26 +0100

    activity3

---
+++ activity3.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+file = pd.read_excel('linear_data.csv')
+x_axis = file['X']
+y_axis = file['Y']
+
+plt.plot(x_axis, y_axis)
+plt.xlabel("X-Axis")
+plt.ylabel("Y-Axis")
+plt.show()
