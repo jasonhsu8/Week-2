ab85eea689e87275dee9e7804d42945c83c2c670
jasonhsu8 <jace.hsu@gmail.com>
2023-10-12 11:00:39 +0100

    end of lab4

--- activity3.py
+++ activity3.py
@@ -1,11 +1,7 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 
 file = pd.read_csv('linear_data.csv')
-x_axis = file['X']
-y_axis = file['Y']
-
-plt.plot(x_axis, y_axis)
-plt.xlabel("X-Axis")
-plt.ylabel("Y-Axis")
+file.plot(kind="scatter", x = "X", y = "Y")
 plt.show()
